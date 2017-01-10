21b864b6755ac37919f1e17a55592dcb9a99d9b9
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-01-10 16:08:12 +0100

    commit ;-)

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
+  let reportCardUrl = 'https://www.codeschool.com/users/Irvyne.json';
 
-  // your code will go here
+  $.ajax({
+    url: reportCardUrl,
+    dataType: 'jsonp',
+    success: function(response) {
+      let completedCourses = response.courses.completed;
+      let badgesDOM = $('#badges');
 
+      $.each(completedCourses, function(index, badge) {
+        badgesDOM.append(
+          `<div class="course">
+            <h3>${badge.title}</h3>
+            <img src="${badge.badge}" alt="${badge.title}">
+            <a target="_blank" href="${badge.url}" class="btn btn-primary">See Course</a>
+          </div>`
+        );
+      });
+    }
+  });
 });
